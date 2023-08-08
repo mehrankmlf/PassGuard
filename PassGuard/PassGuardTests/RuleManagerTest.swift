8d65130d4bcadf920e9003108d417b6f76e219e1
“Mehran <m.kamalifard>
2023-08-08 15:01:09 +0330

    Finished rule manager unit test

---
+++ PassGuard/PassGuardTests/RuleManagerTest.swift
@@ -0,0 +1,40 @@
+//
+//  RuleManagerTest.swift
+//  PassGuardTests
+//
+//  Created by Mehran Kamalifard on 8/8/23.
+//
+
+import XCTest
+@testable import PassGuard
+
+final class RuleManagerTest: XCTestCase {
+   
+   var sut: Manageable!
+   
+   override func setUp() {
+      sut = RuleManagement()
+   }
+   
+   override func tearDown() {
+      sut = nil
+      super.tearDown()
+   }
+   
+   func testRuleManager_checkScore_ShouldReturnState() {
+      
+      XCTAssertEqual(sut.check("Helloword"), .weak)
+      XCTAssertEqual(sut.check("Helloword!@#"), .veryStrong)
+      XCTAssertEqual(sut.check("Helloword233"), .strong)
+   }
+   
+   func testRuleManager_checkStrenghtMeter_ShouldReturnState() {
+      
+      XCTAssertEqual(sut.strenghtMeter(0), .veryWeak)
+      XCTAssertEqual(sut.strenghtMeter(30), .weak)
+      XCTAssertEqual(sut.strenghtMeter(60), .strong)
+      XCTAssertEqual(sut.strenghtMeter(85), .veryStrong)
+      XCTAssertEqual(sut.strenghtMeter(101), .veryStrong)
+      XCTAssertEqual(sut.strenghtMeter(-10), .veryWeak)
+   }
+}
